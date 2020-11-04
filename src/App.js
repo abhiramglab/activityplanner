5790cb8b5f30f00de2d959a9bb0ea48afb94c932
abhiramglab <gottumukkala.cs18@bmsce.ac.in>
2020-11-04 22:33:17 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 400
+      }
+    }
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route:'signin'
+    }
+  }
+  onRouteChange = (route) => {
+    this.setState({route: route});
+  }
+  render(){
+    return(
+      <div className="App">
+        <Particles className='particles'
+         params={particlesOptions}
+        />
+        
+        { this.state.route === 'home' 
+          ? <div>
+              <Navigation onRouteChange={this.onRouteChange}/>
+            </div>
+          : (
+              this.state.route === 'signin' 
+              ? <Signin onRouteChange={this.onRouteChange} />
+              : <Register onRouteChange={this.onRouteChange} />
+            )
+          
+          
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
